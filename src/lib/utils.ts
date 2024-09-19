3f5a52597899849247d7e8223495c50c0298dd72
em snook <michael.snook@gmail.com>
2024-09-19 16:41:21 +0530

    Refactor the mapArray to be better with its keys and unknown types

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,18 +6,22 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 // this is type-funky bc we're using dynamic keys (TODO consider Map)
-export function mapArray<T, K extends keyof T>(arr: Array<T>, key: string) {
+export function mapArray<T extends Record<string, any>, K extends keyof T>(
+	arr: Array<T> | null | undefined,
+	key: K
+) {
 	if (!key) throw new Error('Must provide a key to map against')
-	if (!arr) return null // uninitialized or null array
-	// @ts-ignore
-	const result: { [key: K]: T } = {}
-	// @ts-ignore
-	const valids = arr.filter((item: T) => typeof item[key] === 'string')
-	// return empty map
-	if (!valids.length) return {}
-	valids.forEach((item) => {
-		// @ts-ignore
-		result[item[key]] = item
-	})
-	return result
+	if (arr === undefined) return undefined
+	if (!arr) return {} // uninitialized or null array returns empty object
+
+	return arr.reduce(
+		(result, item) => {
+			const itemKey = item[key]
+			if (typeof itemKey === 'string') {
+				result[itemKey] = item
+			}
+			return result
+		},
+		{} as Record<string, T>
+	)
 }
