5b0ffec3317aa3b1c0283477a8da63a127d8b720
em snook <michael.snook@gmail.com>
2024-09-19 20:32:52 +0530

    Redo the login form and redirect for better type safety

--- src/routes/learn.tsx
+++ src/routes/learn.tsx
@@ -19,7 +19,7 @@ export const Route = createFileRoute('/learn')({
 					// Use the current location to power a redirect after login
 					// (Do not use `router.state.resolvedLocation` as it can
 					// potentially lag behind the actual current location)
-					redirect: location.href,
+					redirectedFrom: location.href,
 				},
 			})
 		}
