d47e17c76404e2612c5da94f7932f57732dafa95
em snook <michael.snook@gmail.com>
2024-10-02 18:22:05 +0530

    Use some more shadcn things on this form

--- src/components/select-multiple-languages.tsx
+++ src/components/select-multiple-languages.tsx
@@ -2,14 +2,12 @@ import type { ChangeEvent } from 'react'
 import languages from 'lib/languages'
 
 interface SelectMultipleLanguagesInputProps {
-	label?: string
 	selectedLanguages: string[]
 	setSelectedLanguages: (languages: string[]) => void
 	except?: string
 }
 
 export default function SelectMultipleLanguagesInput({
-	label = 'Do you know other languages?',
 	selectedLanguages,
 	setSelectedLanguages,
 	except,
@@ -41,30 +39,25 @@ export default function SelectMultipleLanguagesInput({
 	}
 
 	return (
-		<>
-			<label htmlFor="languages_spoken" className="font-bold">
-				{label}
-			</label>
-			<div className="h-40 overflow-auto rounded border py-3">
-				{Object.keys(languages).map((k) => (
-					<p key={`languages-spoken-${k}`} className="flex">
-						<label className="w-full px-3 py-1 has-[:checked]:bg-primary has-[:checked]:text-white">
-							<input
-								type="checkbox"
-								className="mr-2 rounded"
-								value={k}
-								name="languages_spoken"
-								onChange={handleChange}
-								checked={
-									(selectedLanguages && selectedLanguages.indexOf(k) !== -1) ||
-									k === except
-								}
-							/>
-							{languages[k]}
-						</label>
-					</p>
-				))}
-			</div>
-		</>
+		<div className="h-40 overflow-auto rounded border py-3">
+			{Object.keys(languages).map((k) => (
+				<p key={`languages-spoken-${k}`} className="flex">
+					<label className="w-full px-3 py-1 has-[:checked]:bg-primary has-[:checked]:text-white">
+						<input
+							type="checkbox"
+							className="mr-2 rounded"
+							value={k}
+							name="languages_spoken"
+							onChange={handleChange}
+							checked={
+								(selectedLanguages && selectedLanguages.indexOf(k) !== -1) ||
+								k === except
+							}
+						/>
+						{languages[k]}
+					</label>
+				</p>
+			))}
+		</div>
 	)
 }
