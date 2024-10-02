fd9a3300ec206d230e158c8a7a899dc3417dddae
em snook <michael.snook@gmail.com>
2024-10-02 18:34:24 +0530

    Fix validation bug

--- src/components/field-info.tsx
+++ src/components/field-info.tsx
@@ -1,12 +1,9 @@
 import type { FieldApi } from '@tanstack/react-form'
 export function FieldInfo({ field }: { field: FieldApi<any, any, any, any> }) {
-	return (
-		<>
-			{field.state.meta.errors.length ?
-				<em className="text-error text-sm py-1">
-					{field.state.meta.errors.join(',')}
-				</em>
-			:	null}
-		</>
-	)
+	// console.log(`Field info:`, field.state.meta)
+	return field.state.meta.errors.length ?
+			<em className="text-error text-sm py-1">
+				{field.state.meta.errors.join(',')}
+			</em>
+		:	null
 }
