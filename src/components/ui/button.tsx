393d33136160b8ef23c78db40ef38398a0552ee0
em snook <michael.snook@gmail.com>
2024-10-05 00:32:48 +0530

    Clean up the getting-started and the buttons

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,12 +5,12 @@ import { cn } from 'lib/utils'
 import { ButtonHTMLAttributes, forwardRef } from 'react'
 
 const buttonVariants = cva(
-	'inline-flex items-center justify-center whitespace-nowrap rounded text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 disabled:pointer-events-none disabled:opacity-50',
+	'inline-flex items-center justify-center whitespace-nowrap rounded text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 disabled:pointer-events-none disabled:opacity-50',
 	{
 		variants: {
 			variant: {
 				default:
-					'bg-primary text-white hover:opacity-70 aria-expanded:bg-primary transition-opacity',
+					'bg-primary text-white hover:opacity-70 aria-expanded:bg-primary',
 				soft: 'bg-base-300 hover:bg-primary/30',
 				ghost: 'hover:bg-base-100/20 dark:hover:bg-white/10',
 				outline:
@@ -18,14 +18,14 @@ const buttonVariants = cva(
 				white:
 					'bg-white hover:bg-primary text-primary hover:text-white border-primary border-2',
 				action:
-					'bg-primary text-white border-primary border-2 font-semibold sm:min-w-[60%] sm:mx-auto hover:opacity-90 hover:ring',
+					'bg-primary text-white border-primary border-2 hover:opacity-90 hover:ring',
 				destructive:
 					'bg-error text-white border-error border-2 hover:opacity-90 hover:ring',
 			},
 			size: {
 				default: 'h-10 px-4 py-2',
 				sm: 'h-10 rounded px-3 text-sm',
-				lg: 'h-11 rounded-md px-8 text-lg btn-lg',
+				lg: 'rounded-md px-8 py-4 text-xl font-medium',
 				icon: 'h-6 w-6 rounded-full',
 			},
 		},
