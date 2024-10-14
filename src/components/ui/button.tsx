9a3c1a31b02ee4a1e18aa3d1ca13595be85ddae2
em snook <michael.snook@gmail.com>
2024-10-15 01:06:06 +0530

    Refresh two shadcn components

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,32 +1,33 @@
+import * as React from 'react'
 import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
 
 import { cn } from 'lib/utils'
-import { ButtonHTMLAttributes, forwardRef } from 'react'
 
 const buttonVariants = cva(
-	'inline-flex items-center justify-center whitespace-nowrap rounded text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 disabled:pointer-events-none disabled:opacity-50',
+	'inline-flex items-center justify-center whitespace-nowrap rounded text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
 	{
 		variants: {
 			variant: {
 				default:
-					'bg-primary text-white hover:opacity-70 aria-expanded:bg-primary',
+					'bg-primary text-primary-foreground hover:bg-primary/90 aria-expanded:bg-primary',
 				secondary:
 					'bg-foreground/10 dark:bg-background/10 hover:bg-primary/30 dark:hover:bg-primary/40',
 				destructive:
-					'bg-destructive text-white hover:opacity-70 aria-expanded:bg-primary',
+					'bg-destructive text-destructive-foreground hover:bg-destructive/90',
 				ghost:
 					'hover:bg-foreground/10 dark:hover:bg-background/20 text-muted-foreground',
 				outline:
-					'border-primary border focus-within:ring active:ring text-primary dark:text-white hover:bg-primary/30',
+					'border border border-primary text-primary ring-1 bg-background hover:bg-primary/30',
+				link: 'text-primary underline-offset-4 hover:underline',
 				white:
-					'bg-white hover:bg-primary text-primary hover:text-white border-primary border-2',
+					'bg-white hover:bg-primary text-primary hover:text-primary-foreground ring-1 border border-primary',
 			},
 			size: {
 				default: 'h-10 px-4 py-2',
-				sm: 'h-10 rounded px-3 text-sm',
+				sm: 'h-9 rounded-md px-3',
 				lg: 'rounded-md px-8 py-4 text-xl font-medium',
-				icon: 'h-6 w-6 rounded-full',
+				icon: 'h-10 w-10 rounded-full',
 			},
 		},
 		defaultVariants: {
@@ -37,12 +38,12 @@ const buttonVariants = cva(
 )
 
 export interface ButtonProps
-	extends ButtonHTMLAttributes<HTMLButtonElement>,
+	extends React.ButtonHTMLAttributes<HTMLButtonElement>,
 		VariantProps<typeof buttonVariants> {
 	asChild?: boolean
 }
 
-const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 	({ className, variant, size, asChild = false, ...props }, ref) => {
 		const Comp = asChild ? Slot : 'button'
 		return (
