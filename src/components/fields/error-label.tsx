fd9a3b5e5798a86c91eee3b17c89c9b44c1e8cd2
em snook <michael.snook@gmail.com>
2024-10-16 14:55:39 +0530

    Don't render empty errors

--- src/components/fields/error-label.tsx
+++ src/components/fields/error-label.tsx
@@ -1,7 +1,7 @@
 import type { FieldError } from 'react-hook-form'
 
 export default function ErrorLabel(error: FieldError) {
-	return !error ? null : (
+	return !error.message ? null : (
 			<p className="text-sm text-destructive mt-2">{error.message}</p>
 		)
 }
