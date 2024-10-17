aa4356dd3379f5c2932be430c4d0e19e4a6965ee
em snook <michael.snook@gmail.com>
2024-10-17 20:18:10 +0530

    Add theme provider and change tw to use class method

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import Loading from 'components/loading'
 import { AuthProvider } from 'components/auth-context'
+import { ThemeProvider } from 'components/theme-provider'
 import { ShowError } from 'components/errors'
 
 import { routeTree } from './routeTree.gen'
@@ -38,10 +39,12 @@ declare module '@tanstack/react-router' {
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<AuthProvider>
-				<Routes router={router} />
-			</AuthProvider>
-		</QueryClientProvider>
+		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+			<QueryClientProvider client={queryClient}>
+				<AuthProvider>
+					<Routes router={router} />
+				</AuthProvider>
+			</QueryClientProvider>
+		</ThemeProvider>
 	</StrictMode>
 )
