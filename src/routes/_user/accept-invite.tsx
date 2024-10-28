54feb64e92709d17d198d0e9d641d872eef338ef
em snook <michael.snook@gmail.com>
2024-10-28 23:07:02 +0530

    Show different UI states in the accept-invite page

--- src/routes/_user/accept-invite.tsx
+++ src/routes/_user/accept-invite.tsx
@@ -1,5 +1,8 @@
+import { ShowError } from '@/components/errors'
 import Loading from '@/components/loading'
-import { Button } from '@/components/ui/button'
+import SuccessCheckmark from '@/components/SuccessCheckmark'
+import { Button, buttonVariants } from '@/components/ui/button'
+import Callout from '@/components/ui/callout'
 import {
 	Card,
 	CardContent,
@@ -13,7 +16,7 @@ import supabase from '@/lib/supabase-client'
 import { useProfile, usePublicProfile } from '@/lib/use-profile'
 import { useMutation } from '@tanstack/react-query'
 
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 import { ArrowRightLeft } from 'lucide-react'
 import toast from 'react-hot-toast'
 import { z } from 'zod'
@@ -27,12 +30,11 @@ const SearchSchema = z.object({
 
 type SearchType = z.infer<typeof SearchSchema>
 
-const AcceptInviteFormSubmissionSchema = SearchSchema.extend({
-	action: z.enum(['rejected', 'accepted']),
-})
-
-type AcceptInviteFormSubmission = z.infer<
-	typeof AcceptInviteFormSubmissionSchema
+type AcceptInviteFormSubmission = Omit<
+	SearchType & {
+		action_type: 'accepted' | 'rejected'
+	},
+	'lang'
 >
 
 export const Route = createFileRoute('/_user/accept-invite')({
@@ -71,6 +73,55 @@ function AcceptInvitePage() {
 			console.log(`The error accepting the friend invite:`, error)
 		},
 	})
+
+	const AcceptInviteForm = () => {
+		return (
+			<>
+				{learner.avatar_url ?
+					<div className="relative flex flex-row h-44 justify-around items-center gap-4 max-w-[400px] mx-auto">
+						<img
+							src={learner.avatar_url}
+							width=""
+							className="mx-auto rounded-xl max-w-32 flex-shrink"
+							alt={`${learner.username}'s profile picture`}
+						/>
+						{friend.avatar_url ?
+							<>
+								<ArrowRightLeft className="opacity-70 mx-auto" />
+								<img
+									src={learner.avatar_url}
+									className="mx-auto rounded-xl flex-shrink max-w-32"
+									alt={`${learner.username}'s profile picture`}
+								/>
+							</>
+						:	null}
+					</div>
+				:	null}
+				<div className="flex gap-4 flex-row justify-center">
+					<Button
+						size="lg"
+						onClick={() =>
+							acceptOrRejectMutation.mutate({ action: 'accepted' })
+						}
+						disabled={acceptOrRejectMutation.isPending}
+					>
+						Accept invitation
+					</Button>
+					<Button
+						size="lg"
+						variant="secondary"
+						onClick={() =>
+							acceptOrRejectMutation.mutate({ action: 'rejected' })
+						}
+						disabled={acceptOrRejectMutation.isPending}
+					>
+						Ignore
+					</Button>
+				</div>
+			</>
+		)
+	}
+
 	return (
 		<main className="p-2 w-app flex flex-col justify-center h-screen pb-20">
 			{isPending ?
@@ -85,51 +136,44 @@ function AcceptInvitePage() {
 						</CardDescription>
 					</CardHeader>
 					<CardContent className="space-y-4">
-						{learner.avatar_url ?
-							<div className="relative flex flex-row h-44 justify-around items-center gap-4 max-w-[400px] mx-auto">
-								<img
-									src={learner.avatar_url}
-									width=""
-									className="mx-auto rounded-xl max-w-32 flex-shrink"
-									alt={`${learner.username}'s profile picture`}
-								/>
-								{friend.avatar_url ?
-									<>
-										<ArrowRightLeft className="opacity-70 mx-auto" />
-										<img
-											src={learner.avatar_url}
-											width=""
-											className="mx-auto rounded-xl flex-shrink max-w-32"
-											alt={`${learner.username}'s profile picture`}
-										/>
-									</>
-								:	null}
-							</div>
-						:	null}
-						<div className="flex gap-4 flex-row justify-center">
-							<Button
-								size="lg"
-								onClick={() =>
-									acceptOrRejectMutation.mutate({ action: 'accepted' })
-								}
-								disabled={acceptOrRejectMutation.isPending}
-							>
-								Accept invitation
-							</Button>
-							<Button
-								size="lg"
-								variant="secondary"
-								onClick={() =>
-									acceptOrRejectMutation.mutate({ action: 'rejected' })
-								}
-								disabled={acceptOrRejectMutation.isPending}
-							>
-								Ignore
-							</Button>
-						</div>
+						{acceptOrRejectMutation.error ?
+							<ShowError show={!!acceptOrRejectMutation.error}>
+								<p className="font-bold text-destructive-foreground h5">
+									Something went wrong...
+								</p>
+								<p>{acceptOrRejectMutation.error?.message}</p>
+							</ShowError>
+						: !acceptOrRejectMutation.isSuccess ?
+							<AcceptInviteForm />
+						: acceptOrRejectMutation.variables.action === 'accepted' ?
+							<ShowAccepted />
+						:	<ShowRejected />}
 					</CardContent>
 				</Card>
 			}
 		</main>
 	)
 }
+
+const ShowAccepted = () => (
+	<Callout>
+		<SuccessCheckmark />
+		<div>
+			<h2 className="h3">Okay! You're connected.</h2>
+			<p>
+				<Link className={buttonVariants({ variant: 'default' })}>
+					Maybe go look at their deck?
+				</Link>
+			</p>
+		</div>
+	</Callout>
+)
+
+const ShowRejected = () => (
+	<Callout variant="ghost">
+		<div>
+			<h2 className="h4">Invitation ignored</h2>
+			<p>We won't show you this invitation again.</p>
+		</div>
+	</Callout>
+)
