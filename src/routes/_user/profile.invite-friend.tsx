40f3a17fd05cc3e4475140817ff93524e185ab4e
em snook <michael.snook@gmail.com>
2024-10-30 03:39:29 +0530

    Add a search box to the invite-friend page

--- src/routes/_user/profile.invite-friend.tsx
+++ src/routes/_user/profile.invite-friend.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Controller, useForm } from 'react-hook-form'
@@ -17,14 +17,18 @@ import { Input } from 'components/ui/input'
 import { Label } from 'components/ui/label'
 import { useFriendsInvited } from 'lib/friends'
 import Loading from 'components/loading'
-import { User, X } from 'lucide-react'
+import { Loader2, Search, User, X } from 'lucide-react'
 import supabase from 'lib/supabase-client'
 import {
 	FriendRequestAction,
 	FriendRequestActionInsert,
+	PublicProfile,
 	uuid,
 } from 'types/main'
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
+import { useDebounce } from '@/lib/use-debounce'
+import { AvatarIconRow } from '@/components/ui/avatar-icon'
+import { useAuth } from '@/lib/hooks'
 
 export const Route = createFileRoute('/_user/profile/invite-friend')({
 	component: InviteFriendPage,
@@ -39,9 +43,101 @@ export const Route = createFileRoute('/_user/profile/invite-friend')({
 function InviteFriendPage() {
 	return (
 		<main className="flex flex-col gap-6">
+			<SearchProfiles />
 			<InviteFriendForm />
 			<PendingRequestsSection />
 		</main>
+	)
+}
+
+export default function SearchProfiles() {
+	const navigate = Route.useNavigate()
+	const { query } = Route.useSearch()
+	const [inputValue, setInputValue] = useState(query)
+	const { userId } = useAuth()
+	const debouncedSearchTerm = useDebounce(inputValue, 300)
+
+	const {
+		data: searchResults,
+		isPending,
+		error,
+	} = useQuery({
+		queryKey: ['searchProfiles', debouncedSearchTerm],
+		queryFn: async (): Promise<PublicProfile[]> => {
+			if (!debouncedSearchTerm) return []
+			const { data, error } = await supabase
+				.from('public_profile')
+				.select('uid, username, avatar_url')
+				.ilike('username', `%${debouncedSearchTerm}%`)
+				.limit(10)
+
+			if (error) throw error
+			return data || []
+		},
+		enabled: debouncedSearchTerm.length > 0,
+	})
+
+	const handleInputChange = (value: string) => {
+		setInputValue(value)
+		navigate({
+			to: '.',
+			search: (prev) => ({ ...prev, query: value }),
+			replace: true,
+		})
+	}
+
+	const requestFriend = async (friendId: string) => {
+		const {
+			data: { user },
+		} = await supabase.auth.getUser()
+		if (!user) throw new Error('User not authenticated')
+
+		const { error } = await supabase
+			.from('friend_request_action')
+			.insert({ uid_from: userId, uid_to: friendId, status: 'requested' })
+
+		if (error) throw error
+		toast.success('Friend request sent successfully')
+	}
+
+	if (error) {
+		toast.error('Failed to search profiles')
+	}
+
+	return (
+		<Card>
+			<CardHeader>
+				<CardTitle>Search Profiles</CardTitle>
+			</CardHeader>
+			<CardContent>
+				{isPending ?
+					<div className="flex justify-center mt-4">
+						<Loader2 className="h-6 w-6 animate-spin" />
+					</div>
+				:	<div>
+						<form className="flex flex-row gap-2">
+							<Input
+								placeholder="Search by username"
+								onChange={(event) => {
+									handleInputChange(event.target.value)
+								}}
+							/>
+							<Button>
+								<Search />
+								<span className="hidden @md:block">Search</span>
+							</Button>
+						</form>
+						{searchResults.map((profile) => (
+							<AvatarIconRow {...profile}>
+								<Button onClick={() => requestFriend(profile.uid)}>
+									Add as Friend
+								</Button>
+							</AvatarIconRow>
+						))}
+					</div>
+				}
+			</CardContent>
+		</Card>
 	)
 }
 
@@ -150,19 +246,7 @@ function PendingRequestsSection() {
 				:	data.map((invite) => {
 						return hiddenRequests.indexOf(invite.uid_to) > -1 ?
 								null
-							:	<div
-									key={invite.uid_to}
-									className="flex flex-row justify-between items-center gap-4"
-								>
-									{invite.friend.avatar_url ?
-										<img
-											src={invite.friend.avatar_url}
-											aria-disabled="true"
-											alt={`${invite.friend.username}'s avatar`}
-										/>
-									:	<User className="bg-foreground/20 rounded-full w-8 h-8 p-1" />
-									}
-									<p className="me-auto">{invite.friend.username}</p>
+							:	<AvatarIconRow {...invite.friend} key={invite.uid_to}>
 									<Button
 										variant="secondary"
 										className="w-8 h-8"
@@ -172,7 +256,7 @@ function PendingRequestsSection() {
 									>
 										<X className="w-6 h-6 p-0" />
 									</Button>
-								</div>
+								</AvatarIconRow>
 					})
 				}
 			</CardContent>
