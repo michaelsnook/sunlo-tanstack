fd25eb26e8321226bd19189e5bee8b7384f42b10
em snook <michael.snook@gmail.com>
2024-10-30 03:39:29 +0530

    Add a search box to the invite-friend page

---
+++ src/components/ui/avatar-icon.tsx
@@ -0,0 +1,27 @@
+import { PublicProfile } from '@/types/main'
+import { ReactNode } from '@tanstack/react-router'
+import { User } from 'lucide-react'
+
+type AvatarIconRowProps = PublicProfile & {
+	children?: ReactNode
+}
+
+export function AvatarIconRow({
+	avatar_url,
+	username,
+	children,
+}: AvatarIconRowProps) {
+	return (
+		<div className="flex flex-row justify-between items-center gap-4">
+			{avatar_url ?
+				<img
+					src={avatar_url}
+					aria-disabled="true"
+					alt={`${username}'s avatar`}
+				/>
+			:	<User className="bg-foreground/20 rounded-full w-8 h-8 p-1" />}
+			<p className="me-auto">{username}</p>
+			{children}
+		</div>
+	)
+}
