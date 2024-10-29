7c6a75e69201eebb96c49b1b5434fb0552ed6993
em snook <michael.snook@gmail.com>
2024-10-29 19:16:30 +0530

    Add a static version of the 'review pending invites' card
    It does fetch the pending invites, but the "cancel invite" button
    is not wired up with a mutation or mutation state UI.

--- src/routes/learn/$lang/invite-friend.tsx
+++ src/routes/learn/$lang/invite-friend.tsx
@@ -15,6 +15,9 @@ import {
 } from 'components/ui/card'
 import { Input } from 'components/ui/input'
 import { Label } from 'components/ui/label'
+import { useFriendsInvited } from 'lib/friends'
+import Loading from 'components/loading'
+import { User, X } from 'lucide-react'
 
 export const Route = createFileRoute('/learn/$lang/invite-friend')({
 	component: InviteFriendTab,
@@ -45,31 +48,72 @@ function InviteFriendTab() {
 	})
 
 	return (
+		<main className="flex flex-col gap-6">
+			<Card>
+				<CardHeader>
+					<CardTitle>Invite a Friend</CardTitle>
+					<CardDescription>
+						Learn together with a friend who can help you practice.
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<form onSubmit={onSubmit} className="space-y-4">
+						<div>
+							<Label htmlFor="email">Friend's Email</Label>
+							<Controller
+								name="email"
+								control={control}
+								render={({ field }) => (
+									<Input
+										{...field}
+										type="email"
+										placeholder="Enter your friend's email"
+									/>
+								)}
+							/>
+						</div>
+						<Button type="submit">Send Invitation</Button>
+					</form>
+				</CardContent>
+			</Card>
+			<PendingRequestsSection />
+		</main>
+	)
+}
+
+function PendingRequestsSection() {
+	const { data, isPending } = useFriendsInvited()
+	return (
 		<Card>
 			<CardHeader>
-				<CardTitle>Invite a Friend</CardTitle>
-				<CardDescription>
-					Learn together with a friend who can help you practice.
-				</CardDescription>
+				<CardTitle>Pending invites</CardTitle>
 			</CardHeader>
 			<CardContent>
-				<form onSubmit={onSubmit} className="space-y-4">
-					<div>
-						<Label htmlFor="email">Friend's Email</Label>
-						<Controller
-							name="email"
-							control={control}
-							render={({ field }) => (
-								<Input
-									{...field}
-									type="email"
-									placeholder="Enter your friend's email"
-								/>
-							)}
-						/>
-					</div>
-					<Button type="submit">Send Invitation</Button>
-				</form>
+				{isPending ?
+					<Loading />
+				:	data.map((invite) => {
+						return (
+							<div className="flex flex-row justify-between items-center gap-2">
+								{invite.friend.avatar_url ?
+									<img
+										src={invite.friend.avatar_url}
+										aria-disabled="true"
+										alt={`${invite.friend.username}'s avatar`}
+									/>
+								:	<User className="bg-foreground/20 rounded-full w-8 h-8 p-1" />}
+								<p className="me-auto">{invite.friend.username}</p>
+								<Button
+									variant="secondary"
+									className="w-8 h-8"
+									size="icon"
+									title="Cancel pending invitation"
+								>
+									<X className="w-6 h-6 p-0" />
+								</Button>
+							</div>
+						)
+					})
+				}
 			</CardContent>
 		</Card>
 	)
