e70edd33d7cc9ab229c3b46441595258603b0bd4
em snook <michael.snook@gmail.com>
2024-11-24 14:48:39 +0530

    Add a bit of nav to the friends index page

--- src/routes/_user/friends.index.tsx
+++ src/routes/_user/friends.index.tsx
@@ -2,7 +2,7 @@ import { createFileRoute, Link } from '@tanstack/react-router'
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { buttonVariants } from '@/components/ui/button'
-import { Loader2, Search } from 'lucide-react'
+import { HeartHandshake, Loader2, Search } from 'lucide-react'
 import { useRelations } from '@/lib/friends'
 import { ShowError } from '@/components/errors'
 import { ProfileWithRelationship } from '@/components/profile-with-relationship'
@@ -51,59 +51,81 @@ function PendingRequestsSection() {
 function FriendProfiles() {
 	const { data, isPending, error } = useRelations()
 	return (
-		<Card>
-			<CardHeader>
-				<CardTitle>
-					<div className="flex flex-row justify-between items-center">
-						<span>Your friends</span>
-						<Link
-							to="/friends/request"
-							search
-							aria-disabled="true"
-							className={buttonVariants({ size: 'badge', variant: 'outline' })}
-						>
-							<Search className="h-3 w-3" />{' '}
-							<span className="me-1">Search</span>
-						</Link>
-					</div>
-				</CardTitle>
-			</CardHeader>
-			<CardContent>
-				<div className="space-y-2">
-					{isPending ?
-						<Loader2 />
-					: error ?
-						<></>
-					: !(data?.uids.friends?.length > 0) ?
-						<p>
-							Your friends list is empty for now. Use{' '}
+		<>
+			<div className="flex gap-2 flex-row flex-wrap dark">
+				<Link
+					to="/friends/request"
+					from={Route.fullPath}
+					className={buttonVariants({ variant: 'secondary' })}
+				>
+					<Search /> Find friends
+				</Link>
+				<Link
+					to="/friends/invite"
+					from={Route.fullPath}
+					className={buttonVariants({ variant: 'secondary' })}
+				>
+					<HeartHandshake /> Invite friends
+				</Link>
+			</div>
+
+			<Card>
+				<CardHeader>
+					<CardTitle>
+						<div className="flex flex-row justify-between items-center">
+							<span>Your friends</span>
 							<Link
-								className="s-link"
-								from={Route.fullPath}
 								to="/friends/request"
+								search
+								aria-disabled="true"
+								className={buttonVariants({
+									size: 'badge',
+									variant: 'outline',
+								})}
 							>
-								the search screen
-							</Link>{' '}
-							to find friends on Sunlo or{' '}
-							<Link
-								className="s-link"
-								from={Route.fullPath}
-								to="/friends/invite"
-							>
-								invite them to create an account
+								<Search className="h-3 w-3" />{' '}
+								<span className="me-1">Search</span>
 							</Link>
-							.
-						</p>
-					:	data?.uids.friends.map((uid) => (
-							<ProfileWithRelationship
-								key={uid}
-								uid={uid}
-								profile={data?.relationsMap[uid].profile}
-							/>
-						))
-					}
-				</div>
-			</CardContent>
-		</Card>
+						</div>
+					</CardTitle>
+				</CardHeader>
+				<CardContent>
+					<div className="space-y-2">
+						{isPending ?
+							<Loader2 />
+						: error ?
+							<></>
+						: !(data?.uids.friends?.length > 0) ?
+							<p>
+								Your friends list is empty for now. Use{' '}
+								<Link
+									className="s-link"
+									from={Route.fullPath}
+									to="/friends/request"
+								>
+									the search screen
+								</Link>{' '}
+								to find friends on Sunlo or{' '}
+								<Link
+									className="s-link"
+									from={Route.fullPath}
+									to="/friends/invite"
+								>
+									invite them to create an account
+								</Link>
+								.
+							</p>
+						:	data?.uids.friends.map((uid) => (
+								<ProfileWithRelationship
+									key={uid}
+									uid={uid}
+									profile={data?.relationsMap[uid].profile}
+								/>
+							))
+						}
+					</div>
+				</CardContent>
+			</Card>
+		</>
 	)
 }
