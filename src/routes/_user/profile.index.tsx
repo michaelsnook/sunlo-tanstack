09f659fe1a64db9481686a791347b2f827cb0ba0
em snook <michael.snook@gmail.com>
2024-11-24 19:53:19 +0530

    Use slightly more-real friend data for profile page

--- src/routes/_user/profile.index.tsx
+++ src/routes/_user/profile.index.tsx
@@ -1,5 +1,11 @@
 import { buttonVariants } from '@/components/ui/button'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card'
 import { useProfile } from '@/lib/use-profile'
 import { createFileRoute, Link } from '@tanstack/react-router'
 import {
@@ -7,7 +13,8 @@ import {
 	UpdateProfileForm,
 	UserAuthCard,
 } from '@/components/profile'
-import { ArrowRight } from 'lucide-react'
+import { ArrowRight, Loader2 } from 'lucide-react'
+import { useRelations } from '@/lib/friends'
 
 export const Route = createFileRoute('/_user/profile/')({
 	component: ProfilePage,
@@ -45,30 +52,43 @@ function ProfilePage() {
 // TODO the database doesn't have friendships yet so this is all mockup-y
 // and the type is also mocked
 function FriendsSection() {
-	const profileQuery = useProfile()
-	if (profileQuery.data === null) return null
+	const { data, isPending } = useRelations()
 
-	const friends = profileQuery.data?.friendships || []
-
-	return (
-		<Card>
-			<CardHeader>
-				<CardTitle>Your Friends</CardTitle>
-			</CardHeader>
-			<CardContent className="space-y-4">
-				<ul className="list-disc ml-4">
-					<li>mahesh (see recent activity or whatever)</li>
-					<li>a-money (you have a new phrase from them)</li>
-					<li>j-bhai (nothing special actually)</li>
-				</ul>
-				<Link
-					to="/friends/search"
-					from={Route.fullPath}
-					className={buttonVariants({ variant: 'secondary' })}
-				>
-					Invite friends
-				</Link>
-			</CardContent>
-		</Card>
-	)
+	return isPending ?
+			<Loader2 />
+		:	<Card>
+				<CardHeader>
+					<CardTitle>Your Friends</CardTitle>
+					<CardDescription>
+						NB: This is just sample activity; there's no database object yet for
+						friend-viewable user activity
+					</CardDescription>
+				</CardHeader>
+				<CardContent className="space-y-4">
+					<ul className="list-disc ml-4">
+						{data.uids.friends.map((uid) => {
+							const d = data.relationsMap[uid]
+							return !d.profile ? null : (
+									<li key={uid}>
+										<Link
+											to="/friends/$uid"
+											params={{ uid }}
+											className="s-link"
+										>
+											{d.profile.username}
+										</Link>{' '}
+										<span className="opacity-70">was doing something</span>
+									</li>
+								)
+						})}
+					</ul>
+					<Link
+						to="/friends/search"
+						from={Route.fullPath}
+						className={buttonVariants({ variant: 'secondary' })}
+					>
+						Invite friends
+					</Link>
+				</CardContent>
+			</Card>
 }
